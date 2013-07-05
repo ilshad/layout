812e1412bcd2e6453543013632419faa1fbe96ce
Ilshad Khabibullin <astoon@spacta.com>
2013-07-05 13:38:37 +0400

    test fix

--- test/ilshad/layout_test.clj
+++ test/ilshad/layout_test.clj
@@ -2,6 +2,7 @@
   (:require [ilshad.layout :refer [layout wrap-layout prevent-layout]]
             [ring.util.response :refer [response]]
             [ring.mock.request :refer [request]]
+            [net.cgrand.enlive-html :as html]
             [clojure.test :refer [deftest is]]))
 
 (defn handler-1
@@ -16,12 +17,20 @@
             :body "foo"
             :layout nil}))))
 
+(html/deftemplate template-1
+  ""
+  [req content]
+  )
+
 (defn template-1
   [_ content]
-  (str "foo " (str content)))
+  (html/template )
+  (str "foo " content))
 
 (deftest test-layout-response-string
-  (is (= (layout (request :get "/") "bar" template-1)
+  (is (= (layout (request :get "/")
+                 "bar"
+                 template-1)
          {:status 200
           :headers {"Content-Type" "text/html; charset=utf-8"}
           :body "foo bar"})))
