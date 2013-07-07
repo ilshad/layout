bd74445f4c237c4b3d246a0407352c970f862065
Ilshad Khabibullin <astoon@spacta.com>
2013-07-07 23:32:46 +0400

    all tests for enlive in both sides of layout flow

--- test/ilshad/layout_test.clj
+++ test/ilshad/layout_test.clj
@@ -29,13 +29,13 @@
 
 (deftest test-layout-deftemplate-body-string
   (let [resp (layout (request :get "/")
-                     "bar"
+                     "foo"
                      template-1)]
     (is (= (:status resp) 200))
     (is (= (:headers resp) {"Content-Type" "text/html; charset=utf-8"}))
     (is (= (:body resp) '("<html>" "\n  " "<body>" "\n    "
                           "<h1>Title\n    </h1>"
-                          "<div id=\"main\">" "bar" "</div>" "\n  "
+                          "<div id=\"main\">" "foo" "</div>" "\n  "
                           "</body>" "\n\n" "</html>")))))
 
 (deftest test-layout-deftemplate-body-snippet
@@ -44,6 +44,30 @@
                      template-1)]
     (is (= (:status resp) 200))
     (is (= (:headers resp) {"Content-Type" "text/html; charset=utf-8"}))
+    (is (= (:body resp) '("<html>" "\n  " "<body>" "\n    "
+                          "<h1>Title\n    </h1>"
+                          "<div id=\"main\">"
+                          "<div id=\"snippet-1\">foo</div>"
+                          "</div>" "\n  "
+                          "</body>" "\n\n" "</html>")))))
+
+(deftest test-layout-deftemplate-response-string
+  (let [resp (layout (request :get "/")
+                     (response "foo")
+                     template-1)]
+    (is (= (:status resp) 200))
+    (is (= (:headers resp) {}))
+    (is (= (:body resp) '("<html>" "\n  " "<body>" "\n    "
+                          "<h1>Title\n    </h1>"
+                          "<div id=\"main\">" "foo" "</div>" "\n  "
+                          "</body>" "\n\n" "</html>")))))
+
+(deftest test-layout-deftemplate-response-snippet
+  (let [resp (layout (request :get "/")
+                     (response (snippet-1))
+                     template-1)]
+    (is (= (:status resp) 200))
+    (is (= (:headers resp) {}))
     (is (= (:body resp) '("<html>" "\n  " "<body>" "\n    "
                           "<h1>Title\n    </h1>"
                           "<div id=\"main\">"
