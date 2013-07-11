634492812954dcd3053fd3f909da3fb478f721f2
Ilshad Khabibullin <astoon@spacta.com>
2013-07-11 17:58:30 +0400

    enlive deps moved to dev profile

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.2.0-beta2"]
-                 [compojure "1.1.5"]
-                 [enlive "1.1.1"]]
-  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]
+                 [compojure "1.1.5"]]
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]
+                                  [enlive "1.1.1"]]
                    :resource-paths ["test/resources"]}})
