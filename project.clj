945760e4e7cd729205b06d79d07c5e1341fcae5f
Ilshad Khabibullin <astoon@spacta.com>
2013-07-12 21:21:25 +0400

    project meta

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ilshad/layout "0.1.0-SNAPSHOT"
   :description "Web Layout library"
-  :url "https://github.com/ilshad/layout"
+  :url "http://github.com/ilshad/layout"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
